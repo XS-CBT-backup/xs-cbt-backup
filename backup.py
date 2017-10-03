70764cf548a65670bcf148610618b298cdd8ee10
Gabor Igloi <gabor.igloi@citrix.com>
2017-10-03 10:50:54 +0100

    Remove argument guessing to ensure user gets help when no args are specified
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- backup.py
+++ backup.py
@@ -2,20 +2,12 @@
 
 from cbt_tests import CBTTests
 
-def get_only_child(self, path):
-    children = path.iterdir()
-    default = next(children)
-    other = next(children, None)
-    assert(other is None)
-    return default
-
 class Backup:
-    def __init__(self, pool_master=None, username=None, password=None, vm_uuid=None, use_tls=True):
+    def __init__(self, pool_master, username, password, vm_uuid, use_tls=True):
         import urllib
 
         self.init_backup_dir()
 
-        pool_master = pool_master or self.guess_pool_master()
         self._pool_master = pool_master
         # don't use characters that are invalid in filenames
         self._pool_master_dir = self._backup_dir / urllib.parse.quote(pool_master)
@@ -23,26 +15,16 @@ class Backup:
         self._cbt_lib = CBTTests(pool_master=pool_master, username=username, password=password, use_tls=True)
         self._session = self._cbt_lib._session
 
-        vm_uuid = vm_uuid or self.guess_vm_uuid()
         [self._vm] = self._session.xenapi.VM.get_by_uuid(vm_uuid)
         self._vm_dir = self._pool_master_dir / vm_uuid
 
-        username = username or os.environ['XS_USERNAME']
-        password = password or os.environ['XS_PASSWORD']
         self._username = username
         self._password = password
 
     def init_backup_dir(self):
         from pathlib import Path
-        self._backupdir = Path.home() / ".cbt_backups"
-        self._backupdir.mkdir(exist_ok=True)
-
-    def guess_pool_master(self):
-        import urllib
-        return urllib.parse.unquote(get_only_child(self._backupdir))
-
-    def guess_vm_uuid(self):
-        return get_only_child(self._pool_master_dir)
+        self._backup_dir = Path.home() / ".cbt_backups"
+        self._backup_dir.mkdir(exist_ok=True)
 
     def get_backup_dirs(self):
         self._vm_dir.iterdir()
