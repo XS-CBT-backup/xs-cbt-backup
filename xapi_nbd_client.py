3cb8edf18e4dbf9085d6ef3d8a2c79750a7da87d
Gabor Igloi <gabor.igloi@citrix.com>
2017-10-08 11:52:15 +0100

    xapi_nbd_client: enable secure NBD on all networks if it isn't enabled on any
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- xapi_nbd_client.py
+++ xapi_nbd_client.py
@@ -1,9 +1,25 @@
 from new_nbd_client import new_nbd_client
 
 
+def enable_nbd_if_necessary(session):
+	has_nbd_network = False
+	for network in session.xenapi.network.get_all():
+		purpose = session.xenapi.network.get_purpose(network)
+		if "nbd" in purpose or "insecure_nbd" in purpose:
+			print("Found network on which NBD ({}) is allowed: {}".format(purpose, network))
+			has_nbd_network = True
+	if not has_nbd_network:
+		print("WARNING: Found no network on which NBD is allowed, enabling secure NBD on ALL NETWORKS!!!!!!!")
+		for network in session.xenapi.network.get_all():
+			print("Enabling secure NBD on network {}".format(network))
+			session.xenapi.network.add_purpose(network, "nbd")
+
+
 class xapi_nbd_client(new_nbd_client):
     def __init__(self, session, vdi, use_tls=True):
         from pprint import pprint as pp
+
+        enable_nbd_if_necessary(session)
 
         infos = session.xenapi.VDI.get_nbd_info(vdi)
         pp('Can connect to the following addresses:')
