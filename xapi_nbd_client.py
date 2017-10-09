d7e7aeb06e3366af0e56607d054ab5d805fad893
Gabor Igloi <gabor.igloi@citrix.com>
2017-10-09 10:44:55 +0100

    new_nbd_client: add parameter turn off auto-enabling NBD on all networks if necessary
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- xapi_nbd_client.py
+++ xapi_nbd_client.py
@@ -2,24 +2,27 @@ from new_nbd_client import new_nbd_client
 
 
 def enable_nbd_if_necessary(session):
-	has_nbd_network = False
-	for network in session.xenapi.network.get_all():
-		purpose = session.xenapi.network.get_purpose(network)
-		if "nbd" in purpose or "insecure_nbd" in purpose:
-			print("Found network on which NBD ({}) is allowed: {}".format(purpose, network))
-			has_nbd_network = True
-	if not has_nbd_network:
-		print("WARNING: Found no network on which NBD is allowed, enabling secure NBD on ALL NETWORKS!!!!!!!")
-		for network in session.xenapi.network.get_all():
-			print("Enabling secure NBD on network {}".format(network))
-			session.xenapi.network.add_purpose(network, "nbd")
+    has_nbd_network = False
+    for network in session.xenapi.network.get_all():
+        purpose = session.xenapi.network.get_purpose(network)
+        if "nbd" in purpose or "insecure_nbd" in purpose:
+            print("Found network on which NBD ({}) is allowed: {}".format(
+                purpose, network))
+            has_nbd_network = True
+    if not has_nbd_network:
+        print("WARNING: Found no network on which NBD is allowed, enabling "
+              "secure NBD on ALL NETWORKS!!!!!!!")
+        for network in session.xenapi.network.get_all():
+            print("Enabling secure NBD on network {}".format(network))
+            session.xenapi.network.add_purpose(network, "nbd")
 
 
 class xapi_nbd_client(new_nbd_client):
-    def __init__(self, session, vdi, use_tls=True):
+    def __init__(self, session, vdi, use_tls=True, auto_enable_nbd=True):
         from pprint import pprint as pp
 
-        enable_nbd_if_necessary(session)
+        if auto_enable_nbd:
+            enable_nbd_if_necessary(session)
 
         infos = session.xenapi.VDI.get_nbd_info(vdi)
         pp('Can connect to the following addresses:')
@@ -37,4 +40,9 @@ class xapi_nbd_client(new_nbd_client):
             ca_cert = None
 
         new_nbd_client.__init__(
-            self, host=host, export_name=export_name, port=port, ca_cert=ca_cert, subject=subject)
+            self,
+            host=host,
+            export_name=export_name,
+            port=port,
+            ca_cert=ca_cert,
+            subject=subject)
